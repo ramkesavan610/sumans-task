1fccbb04d28f5b13bf8e63d394a2a10cc674d0cb
Ramakrishnan <ramakrishnan@Ramakrishnans-MacBook-Air.local>
2021-06-30 04:14:52 +0530

    changeLog:screenshots  updated

--- resources/views/myOrders.blade.php
+++ resources/views/myOrders.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Product List</div>
+                <div class="panel-heading">My Order List</div>
 
                 <div class="panel-body">
                     
